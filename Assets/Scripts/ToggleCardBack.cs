8de0ca5ecc76866ecc082cd494a7f6676a240680
rafael <rafaelferreiracosta26@gmail.com>
2021-03-08 23:32:43 -0300

    adding card back and toggle hide

---
+++ Assets/Scripts/ToggleCardBack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCardBack : MonoBehaviour
+{
+    public GameObject cardBack;
+
+    private void Start()
+    {
+        string parentName = transform.parent.parent.name;
+
+        if(parentName == "PlayerCardArea")
+        {
+            cardBack.SetActive(false);
+        }
+
+
+    }
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+}
